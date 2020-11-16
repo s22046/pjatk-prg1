f2a406fdfa0805eb6d28ce24c82f1d39ec132873
s22046 <s22046@pjwstk.edu.pl>
2020-11-16 20:53:33 +0100

    Adding simple echo

---
+++ src/s03-echo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+// 
+
+auto main() -> int
+{
+    std::cout << " Hello. \n What can I echo for you?" <<  "\n{Write exit to leave.)\n";
+    auto input = std::string{};
+    auto const exit = std::string{"exit"};
+    do
+    {
+        std::getline(std::cin, input);
+        std::cout << input << " \n";
+
+    } while (input != exit);
+    
+}
