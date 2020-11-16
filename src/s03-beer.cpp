106f306ff28f7bcbd8f70407b066e3c40ad8df48
s22046 <s22046@pjwstk.edu.pl>
2020-11-16 23:36:59 +0100

    Adding beer song

---
+++ src/s03-beer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+// 
+
+auto main() -> int
+{
+    std::cout << "How many bottles of beer?\n";
+    auto input = std::string{};
+    std::getline(std::cin, input);
+    auto startingbottles = std::stoi(input);
+
+    for (int i = startingbottles;i > 0 ;i--)
+    {
+        std::cout << i << " bottles of beer on the wall " << i << " bottles of beer.\n" << " Take one down, pass it around " << i-1 << " bottles of beer on the wall... ";
+    }
+    
+    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bootles of beer on the wall.";
+}
